79637927b6e1ac57f1512a3512f22b1c863ad4c9
TTG3333 <ngassamsimon@gmail.com>
2020-12-16 09:16:24 -0500

    Update to Day 1 and new inputs because of account change

--- 2020/Day1/Challenge 1.py
+++ 2020/Day1/Challenge 1.py
@@ -1,11 +1,10 @@
-F = open("Input.txt", "tr")
-numbers = F.read().split("\n")
-F.close()
-
-for x in numbers:
-    xNum = int(x)
-    num = 2020 - xNum
-    for y in numbers:
-        yNum = int(y)
-        if num == yNum:
-            print(yNum*xNum)
+with open("Input.txt", "tr") as F:
+    numbers = F.read().split("\n")
+    for x in numbers:
+        xNum = int(x)
+        num = 2020 - xNum
+        for y in numbers:
+            yNum = int(y)
+            if num == yNum:
+                print(yNum*xNum)
+    F.close()
--- 2020/Day1/Challenge 2.py
+++ 2020/Day1/Challenge 2.py
@@ -1,12 +1,11 @@
-F = open("Input.txt", "tr")
-numbers = F.read().split("\n")
-F.close()
-
-for x in numbers:
-    xNum = int(x)
-    for y in numbers:
-        yNum = int(y)
-        for z in numbers:
-            zNum = int(z)
-            if xNum + yNum + zNum == 2020:
-                print(xNum*yNum*zNum)
+with open("Input.txt", "tr") as F:
+    numbers = F.read().split("\n")
+    for x in numbers:
+        xNum = int(x)
+        for y in numbers:
+            yNum = int(y)
+            for z in numbers:
+                zNum = int(z)
+                if xNum + yNum + zNum == 2020:
+                    print(xNum*yNum*zNum)
+    F.close()
