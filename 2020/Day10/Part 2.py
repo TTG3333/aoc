3a586dbccb984fcc2f3c7134dfcef6f322af3161
TTG3333 <ngassamsimon@gmail.com>
2021-01-06 21:03:31 -0500

    Completed Day 10 part 2 and updated readme

--- 2020/Day10/Part 2.py
+++ 2020/Day10/Part 2.py
@@ -1,26 +1,22 @@
-import datetime
+def zeroCheck(val):
+    if val < 0:
+        return 0
+    return val
 
-def find_arrangements(joltage, final_joltage, adapters):
-    arrangements = 0
-    if joltage == final_joltage - 3:
-        return 1
-    for y, x in enumerate(adapters[:3]):
-        if x - joltage > 0 and x - joltage < 4:
-            arrangements += find_arrangements(x, final_joltage, adapters[y + 1:])
-        elif x - joltage > 3:
-            break
-    return arrangements
-
-start_time = datetime.datetime.now()
-print(start_time)
 with open("Input.txt", "tr") as F:
     adapters = F.read().splitlines()
-fixed_adapters = []
+fixed_adapters = [0]
 for y, x in enumerate(adapters):
     fixed_adapters.append(int(x))
-fixed_adapters.sort()
-start_joltage = 0
-final_joltage = fixed_adapters[-1] + 3
-answer = find_arrangements(start_joltage, final_joltage, fixed_adapters)
-print(datetime.datetime.now() - start_time)
-print(answer)
+fixed_adapters.sort(reverse=True)
+adapter_data = {}
+for y, x in enumerate(fixed_adapters):
+    arrangements = 0
+    if y == 0:
+        adapter_data[x] = 1
+        continue
+    for z in fixed_adapters[zeroCheck(y-3):y]:
+        if z - x > 0 and z - x < 4:
+            arrangements += adapter_data[z]
+    adapter_data[x] = arrangements
+print(adapter_data[0])
