4d0136ea652b7ca90bfaf674f4ea4659779bba79
TTG3333 <ngassamsimon@gmail.com>
2022-12-04 10:40:17 -0500

    Completed 2022 Day 4

---
+++ 2022/Day 4/Part 2.py
@@ -0,0 +1,10 @@
+with open("Input.txt", "tr") as F:
+    pairs = F.read().splitlines()
+
+total = 0
+for pair in pairs:
+    elves = [[int(y) for y in x.split("-")] for x in pair.split(",")]
+    elf1, elf2 = [set(range(x[0], x[1]+1)) for x in elves]
+    if elf1 & elf2 != set():
+        total += 1
+print(total)
