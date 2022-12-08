a8eecac3d356b58560af6636e8bf55ec573d7335
TTG3333 <65309965+TTG3333@users.noreply.github.com>
2022-12-07 23:09:36 -0500

    Completed 2015 Day 10

---
+++ 2015/Day 10/Part 1.py
@@ -0,0 +1,20 @@
+with open("Input.txt", "tr") as F:
+    nums = F.read()
+
+def lookSay(nums):
+    newNums = ""
+    current = nums[0]
+    count = 1
+    for char in nums[1:]:
+        if char == current:
+            count += 1
+        else:
+            newNums += str(count) + current
+            current = char
+            count = 1
+    newNums += str(count) + current
+    return newNums
+
+for _ in range(40):
+    nums = lookSay(nums)
+print(len(nums))
