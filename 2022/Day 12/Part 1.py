a3c068db73e47c06799ab0532b333cd0e3905814
TTG3333 <65309965+TTG3333@users.noreply.github.com>
2022-12-12 21:36:28 -0500

    Completed 2022 Day 12

---
+++ 2022/Day 12/Part 1.py
@@ -0,0 +1,37 @@
+from string import ascii_lowercase
+
+with open("Input.txt", "tr") as F:
+    lines = F.read().splitlines()
+
+startPos, endPos = None, None
+grid = []
+for y, line in enumerate(lines):
+    if "S" in line:
+        startPos = [line.find("S"), y]
+    if "E" in line:
+        endPos = [line.find("E"), y]
+    grid.append([{"height": ascii_lowercase.find(x), "dist": float("inf")} for x in line])
+grid[startPos[1]][startPos[0]] = {"height": 0, "dist": 0}
+grid[endPos[1]][endPos[0]] = {"height": 25, "dist": float("inf")}
+
+unvisited = {(x, y) for x in range(len(grid[0])) for y in range(len(grid))}
+current = tuple(startPos)
+around = ((-1, 0), (0, 1), (1, 0), (0, -1))
+while tuple(endPos) in unvisited: # Dijkstra
+    dist = grid[current[1]][current[0]]["dist"]
+    elevation = grid[current[1]][current[0]]["height"]
+    for offset in around:
+        newPos = [current[i] + offset[i] for i in range(2)]
+        if newPos[0] < 0 or newPos[0] >= len(grid[0]) or newPos[1] < 0 or newPos[1] >= len(grid):
+            continue
+        if tuple(newPos) in unvisited:
+            if grid[newPos[1]][newPos[0]]["height"] <= elevation + 1:
+                newDist = grid[newPos[1]][newPos[0]]["dist"]
+                grid[newPos[1]][newPos[0]]["dist"] = min(dist+1, newDist)
+    unvisited.remove(tuple(current))
+    if len(unvisited) > 0:
+        current = min(unvisited, key=lambda a: grid[a[1]][a[0]]["dist"])
+    else:
+        break
+
+print(grid[endPos[1]][endPos[0]]["dist"])
